5f62ee430649171c295d3e495eeff6ca71fe69c7
Marco <marco.helmich@live.com>
2019-08-09 16:05:38 -0700

    don't download mockery all the time

--- genMocks.sh
+++ genMocks.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-go get -u github.com/vektra/mockery
+# go get -u github.com/vektra/mockery
 
 rm mocks/*.go
 
